16d65cf3c7318feab74997b60a0c20affb76cb58
felipe <felipealves1550@gmail.com>
2024-03-16 18:21:52 -0300

    Instalando Fortify

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => Hash::make('test123')
+            'password' => Hash::make('test123'),
         ]);
     }
 }
