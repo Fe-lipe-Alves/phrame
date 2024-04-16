18026734541a98adae62053e924e62b775f2d95f
felipe <felipealves1550@gmail.com>
2024-04-16 00:34:34 -0300

    Criando rota de autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,39 +1,19 @@
 <?php
 
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PhotoController;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
-
-Route::post('/sanctum/token', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-});
-
-Route::post('test', function (Request $request) {
-    return response()->json($request->all());
-});
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('auth', AuthController::class)->name('auth.get');
 
     Route::apiResource('/album', AlbumController::class)->names('album');
 
     Route::apiResource('/photo', PhotoController::class)->names('photo');
 
+    Route::post('test', function (Request $request) {
+        return response()->json($request->all());
+    });
 });
